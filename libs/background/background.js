ffc8c6d5423134e946508ac8903203c699a8bc68
Fredrik <fredrik.christenson@gmail.com>
2017-03-12 17:00:03 +0100

    WP

--- libs/background/background.js
+++ libs/background/background.js
@@ -1,39 +1,41 @@
-var commands = [];
-var store = {};
-var tabId;
+var tabs = {};
 
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg, conn) {
+    console.log('conn', conn);
+    console.log('--------------------------');
+    var url = conn.sender && conn.sender.tab && conn.sender.tab.url;
+    if(!tabs[url]) tabs[url] = {store: {}, commands: []};
+    
     if(msg === 'reset') {
-      commands = [];
+      //tabs = {};
       return;
     }
 
     if(msg === 'done') {
-      tabId = conn.sender.tab.url;
-      if(commands.length > 0) chrome.tabs.query({url: tabId}, runInTab);
+      if(tabs[url].commands.length > 0) chrome.tabs.query({url: url}, runInTab);
       return;
     }
 
     // this will get the store and send it to page_done.js
     // so it can be put on the Bookie object
-    if(msg === 'getStore') return port.postMessage(store);
+    if(msg === 'getStore') return port.postMessage(tabs[url].store);
 
     // this sets a key val pair so it can be saved for a page transfer
     if(msg.type === 'setValueOnStore') {
-      store = msg.store;
+      tabs[url].store = msg.store;
       return;
     }
 
     // split on arrow to know what scripts will run on the next page
-    commands = msg.split('->');
+    tabs[url].commands = msg.split('->');
     
-    chrome.tabs.query({url: tabId}, runInTab);
+    chrome.tabs.query({url: url}, runInTab);
 
-    function runInTab(tabs) {
-      if(tabs && tabs[0]) {
-        chrome.tabs.executeScript(tabs[0].id, {
-          code: commands.shift()
+    function runInTab(results) {
+      if(results && results[0]) {
+        chrome.tabs.executeScript(results[0].id, {
+          code: tabs[url].commands.shift()
         });
       }
     }
